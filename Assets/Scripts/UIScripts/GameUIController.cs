dc2330aa7da0b31a9185eb4bfb6c06e09b74da32
SAqsin <aqsin.sulxayev@gmail.com>
2024-02-06 16:35:10 +0400

    Mock Env & Winner UI

--- Assets/Scripts/UIScripts/GameUIController.cs
+++ Assets/Scripts/UIScripts/GameUIController.cs
@@ -9,14 +9,25 @@ public class GameUIController : MonoBehaviour
     [Header("GameOver UI")]
     [SerializeField] private GameObject gameOverUI;
     private VisualElement rootGameOver;
-   
+
+    [Header("Winner UI")]
+    [SerializeField] private GameObject winnerUI;
+    private VisualElement rootWinner;
+
     void Start()
     {
         rootGameOver = gameOverUI.GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("root_container");
+        rootWinner = winnerUI.GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("root_container");
         rootGameOver.Q<Button>("btn_home").clicked += () => GameOverHomeClick();
+        rootWinner.Q<Button>("btn_next").clicked += () => GoNextLevel();
     }
 
     void GameOverHomeClick() {
+        NavigateScene(Constants.START_SCENE_INDEX);
+    }
+
+    void GoNextLevel()
+    {
         NavigateScene(Constants.START_SCENE_INDEX);
     }
 
@@ -27,6 +38,18 @@ public class GameUIController : MonoBehaviour
         }
         else {
             rootGameOver.style.display = DisplayStyle.None;
+        }
+    }
+
+    public void WinnerUIVisibility(bool visibility)
+    {
+        if (visibility)
+        {
+            rootWinner.style.display = DisplayStyle.Flex;
+        }
+        else
+        {
+            rootWinner.style.display = DisplayStyle.None;
         }
     }
 
