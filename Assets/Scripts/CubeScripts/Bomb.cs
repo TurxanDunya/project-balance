34970ac8fb98f9b915873260b8523bce3a164485
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2024-07-24 21:49:49 +0400

    minor code refactorings

--- Assets/Scripts/CubeScripts/Bomb.cs
+++ Assets/Scripts/CubeScripts/Bomb.cs
@@ -21,17 +21,17 @@ public class Bomb : MonoBehaviour
 
         foreach (GameObject affectedObject in affectedObjects)
         {
-            Vector3 magnetPosition = transform.position;
+            Vector3 bombPosition = transform.position;
             Vector3 otherObjectPosition = affectedObject.transform.position;
-            float distanceBetween = Vector3.Distance(magnetPosition, otherObjectPosition);
+            float distanceBetween = Vector3.Distance(bombPosition, otherObjectPosition);
 
             if (distanceBetween <= affectDistance)
             {
                 Destroy(affectedObject);
             }
-
-            Destroy(self);
         }
+
+        Destroy(self);
     }
 
     public void Release()
