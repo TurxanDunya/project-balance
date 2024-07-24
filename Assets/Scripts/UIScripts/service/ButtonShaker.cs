999cfd8ad230d0f2091275790f8e6a784b3daba9
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2024-07-25 00:39:17 +0400

    button shake functionality added

---
+++ Assets/Scripts/UIScripts/service/ButtonShaker.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonShaker : MonoBehaviour
+{
+    public float shakeDuration = 0.5f;
+    public float shakeMagnitude = 5f;
+    public float zoomAmount = 1.1f;
+    public float interval = 2f;
+
+    private Quaternion originalRotation;
+    private Vector3 originalScale;
+
+    private Button button;
+
+    public void ShakeButton(Button button)
+    {
+        this.button = button;
+        originalRotation = button.transform.rotation;
+        originalScale = button.transform.scale;
+
+        StartCoroutine(ShakeAndZoomCoroutine());
+    }
+
+    private IEnumerator ShakeAndZoomCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(interval);
+
+            float elapsed = 0f;
+            while (elapsed < shakeDuration)
+            {
+                float magnitude = SwitchToggle() * shakeMagnitude;
+
+                button.transform.rotation =
+                    originalRotation * Quaternion.Euler(0, 0, magnitude);
+                button.transform.scale = originalScale * zoomAmount;
+
+                elapsed += Time.deltaTime;
+
+                new WaitForSeconds(0.1f);
+                yield return null;
+            }
+
+            button.transform.rotation = originalRotation;
+            button.transform.scale = originalScale;
+        }
+    }
+
+    private bool IsToggleOn = true;
+    private int SwitchToggle()
+    {
+        if(IsToggleOn)
+        {
+            IsToggleOn = false;
+            return 1;
+        }
+        else
+        {
+            IsToggleOn = true;
+            return 0;
+        }
+    }
+}
