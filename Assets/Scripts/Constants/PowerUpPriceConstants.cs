90fa2235805f58b8beff7c27985a27ccc9d006ec
SAqsin <aqsin.sulxayev@gmail.com>
2024-10-30 19:18:59 +0400

    Bomb optimized

--- Assets/Scripts/Constants/PowerUpPriceConstants.cs
+++ Assets/Scripts/Constants/PowerUpPriceConstants.cs
@@ -3,5 +3,5 @@ public class PowerUpPriceConstants
     public const int CHANGE_CUBE = 10;
     public const int ADD_TIME = 20;
     public const int MAGNET = 30;
-    public const int BOMB = 50;
+    public const int BOMB = 0;
 }
