67a081f8f4a34087bdc3e906cd3b27f87cfcbcf6
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2024-11-14 00:08:45 +0400

    lunar console removed from all scenes

--- Assets/Scripts/Core/ApplicationSettings.cs
+++ Assets/Scripts/Core/ApplicationSettings.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using LunarConsolePlugin;
 
 public class ApplicationSettings : MonoBehaviour
 {
     private void Start()
     {
         Application.targetFrameRate = 60;
+
+        LunarConsole.Hide();
+        LunarConsole.Clear();
+        LunarConsole.SetConsoleEnabled(false);
     }
 }
