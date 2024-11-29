c3b79aeb80c1080c06d1a00b3758ca7f669982b4
SAqsin <30318249+SAqsin@users.noreply.github.com>
2024-11-29 10:52:18 +0400

    fix release bugs (#176)

--- Assets/Scripts/Core/ApplicationSettings.cs
+++ Assets/Scripts/Core/ApplicationSettings.cs
@@ -3,24 +3,9 @@ using LunarConsolePlugin;
 
 public class ApplicationSettings : MonoBehaviour
 {
-    [SerializeField] private GameObject lunarConsolePrefab; 
-
     private void Start()
     {
         Application.targetFrameRate = 60;
-
-        CheckLunarConsoleState();
-    }
-
-    private void CheckLunarConsoleState()
-    {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
-        LunarConsole.SetConsoleEnabled(true);
-        Destroy(lunarConsolePrefab);
-#else
-        LunarConsole.SetConsoleEnabled(false);
-        Destroy(lunarConsolePrefab);
-#endif
     }
 
 }
